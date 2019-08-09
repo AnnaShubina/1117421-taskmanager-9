d6544b7c24eaabb7428027dc4b39fa4975bfa8bf
AnnaChernova <ashubina0810@mail.ru>
2019-08-10 00:42:12 +0300

    task 1 ready

---
+++ src/components/load-more.js
@@ -0,0 +1,7 @@
+// components/load-more.js
+
+export const getLoadMoreTemplate = () => {
+  return `
+    <button class="load-more" type="button">load more</button>
+  `.trim();
+};
---
+++ src/components/tasks-container.js
@@ -0,0 +1,9 @@
+// components/tasks-container.js
+
+export const getTasksContainerTemplate = () => {
+  return `
+    <section class="board container">
+      <div class="board__tasks">
+      </div>
+    </section>`.trim();
+};
